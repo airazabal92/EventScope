15086a3ee298e4e97444fc9eaf8f40315cf50138
Jon Kwan <iamjonkwan@gmail.com>
2020-05-05 11:25:37 -0400

    changed the CSS framework for html file

--- Assets/script.js
+++ Assets/script.js
@@ -55,5 +55,12 @@ function getLatLong() {
   }
 
  
-
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.timepicker');
+    var instances = M.Timepicker.init(elems, options);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
 
